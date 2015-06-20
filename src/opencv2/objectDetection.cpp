3aec2e319f44928130aa3f5c6f1cb45425537a17
許邱翔 <wdv4758h@gmail.com>
2015-06-20 19:35:26 +0800

    make OpenCV 2 samples more cleaner
        * add some comments
        * make code less dense

--- src/opencv2/objectDetection.cpp
+++ src/opencv2/objectDetection.cpp
@@ -6,15 +6,21 @@
 
 using namespace cv;
 
+
+
 /** Function Headers */
 void detectAndDisplay( Mat frame );
 
+
+
 /** Global variables */
-String face_cascade_name = "haarcascade_frontalface_alt.xml";
-String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+const String face_cascade_name = "haarcascade_frontalface_alt.xml";
+const String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+const String window_name = "Capture - Face detection";
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
-String window_name = "Capture - Face detection";
+
+
 
 /** @function main */
 int main( void )
@@ -22,30 +28,54 @@ int main( void )
     VideoCapture capture;
     Mat frame;
 
+
+
     //-- 1. Load the cascades
-    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
-    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
+
+    if ( !face_cascade.load( face_cascade_name ) )
+    {
+        printf("--(!)Error loading face cascade\n");
+        return -1;
+    };
+
+    if ( !eyes_cascade.load( eyes_cascade_name ) )
+    {
+        printf("--(!)Error loading eyes cascade\n");
+        return -1;
+    };
+
+
 
     //-- 2. Read the video stream
+
     capture.open( -1 );
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    if ( ! capture.isOpened() )
+    {
+        printf("--(!)Error opening video capture\n");
+        return -1;
+    }
 
     while ( capture.read(frame) )
     {
-        if( frame.empty() )
+        if ( frame.empty() )
         {
             printf(" --(!) No captured frame -- Break!");
             break;
         }
 
         //-- 3. Apply the classifier to the frame
+
         detectAndDisplay( frame );
 
         int c = waitKey(10);
-        if( (char)c == 27 ) { break; } // escape
+
+        if ( (char)c == 27 ) { break; } // escape
     }
+
     return 0;
 }
+
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
@@ -61,8 +91,16 @@ void detectAndDisplay( Mat frame )
 
     for ( size_t i = 0; i < faces.size(); i++ )
     {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        const Point center( faces[i].x + faces[i].width/2,
+                            faces[i].y + faces[i].height/2 );
+
+        ellipse( frame,     // image
+                 center,
+                 Size( faces[i].width/2, faces[i].height/2 ),   // axes
+                 0,                         // angle
+                 0, 360,                    // startAngle, endAngle
+                 Scalar( 255, 0, 255 ),     // color
+                 4, 8, 0 );                 // thickness, lineType, shift
 
         Mat faceROI = frame_gray( faces[i] );
         std::vector<Rect> eyes;
@@ -72,11 +110,19 @@ void detectAndDisplay( Mat frame )
 
         for ( size_t j = 0; j < eyes.size(); j++ )
         {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            const Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2,
+                                    faces[i].y + eyes[j].y + eyes[j].height/2 );
+
             int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+
+            circle( frame,      // image
+                    eye_center,
+                    radius,
+                    Scalar( 255, 0, 0 ),    // color
+                    4, 8, 0 );  // thickness, lineType, shift
         }
     }
+
     //-- Show what you got
     imshow( window_name, frame );
 }
--- src/opencv2/objectDetection2.cpp
+++ src/opencv2/objectDetection2.cpp
@@ -11,15 +11,22 @@
 
 using namespace cv;
 
+
+
 /** Function Headers */
 void detectAndDisplay( Mat frame );
 
+
+
 /** Global variables */
-String face_cascade_name = "lbpcascade_frontalface.xml";
-String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+const String face_cascade_name = "lbpcascade_frontalface.xml";
+const String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+const String window_name = "Capture - Face detection";
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
-String window_name = "Capture - Face detection";
+
+
+
 /**
  * @function main
  */
@@ -28,31 +35,58 @@ int main( void )
     VideoCapture capture;
     Mat frame;
 
+
+
     //-- 1. Load the cascade
-    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
-    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
+    if( !face_cascade.load( face_cascade_name ) )
+    {
+        printf("--(!)Error loading face cascade\n");
+        return -1;
+    };
+
+    if( !eyes_cascade.load( eyes_cascade_name ) )
+    {
+        printf("--(!)Error loading eyes cascade\n");
+        return -1;
+    };
+
+
 
     //-- 2. Read the video stream
+
     capture.open( -1 );
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    if ( ! capture.isOpened() )
+    {
+        printf("--(!)Error opening video capture\n");
+        return -1;
+    }
+
+
 
     while ( capture.read(frame) )
     {
-        if( frame.empty() )
+        if ( frame.empty() )
         {
             printf(" --(!) No captured frame -- Break!");
             break;
         }
 
         //-- 3. Apply the classifier to the frame
+
         detectAndDisplay( frame );
 
         //-- bail out if escape was pressed
+
         int c = waitKey(10);
-        if( (char)c == 27 ) { break; }
+
+        if ( (char)c == 27 ) { break; }
     }
+
     return 0;
 }
+
+
 
 /**
  * @function detectAndDisplay
@@ -68,7 +102,7 @@ void detectAndDisplay( Mat frame )
     //-- Detect faces
     face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0, Size(80, 80) );
 
-    for( size_t i = 0; i < faces.size(); i++ )
+    for ( size_t i = 0; i < faces.size(); i++ )
     {
         Mat faceROI = frame_gray( faces[i] );
         std::vector<Rect> eyes;
@@ -78,18 +112,34 @@ void detectAndDisplay( Mat frame )
         if( eyes.size() == 2)
         {
             //-- Draw the face
-            Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-            ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+            const Point center( faces[i].x + faces[i].width/2,
+                                faces[i].y + faces[i].height/2 );
+
+            ellipse( frame,     // image
+                     center,
+                     Size( faces[i].width/2, faces[i].height/2 ),   // axes
+                     0,                         // angle
+                     0, 360,                    // startAngle, endAngle
+                     Scalar( 255, 0, 0 ),       // color
+                     2, 8, 0 );                 // thickness, lineType, shift
 
             for( size_t j = 0; j < eyes.size(); j++ )
-            { //-- Draw the eyes
-                Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            {
+                //-- Draw the eyes
+                const Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2,
+                                        faces[i].y + eyes[j].y + eyes[j].height/2 );
+
                 int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-                circle( frame, eye_center, radius, Scalar( 255, 0, 255 ), 3, 8, 0 );
+
+                circle( frame,      // image
+                        eye_center,
+                        radius,
+                        Scalar( 255, 0, 255 ),  // color
+                        3, 8, 0 );  // thickness, lineType, shift
             }
         }
-
     }
+
     //-- Show what you got
     imshow( window_name, frame );
 }
